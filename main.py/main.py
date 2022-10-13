578583c7bc6ee3d966fc5d6c5a8c5f41edcc56c5
Jordin Bryant <jordintaylor095@gmail.com>
2022-10-12 22:37:45 -0400

    first commit

---
+++ main.py/main.py
@@ -0,0 +1,51 @@
+# CHALLENGE 1
+# Write a function named sum_to that accepts a single integer, n, and returns the sum of the integers from 1 to n. For example:
+
+# total = 0
+# def sum_to(n):
+#  print(sum(range(1, n+1)))
+
+# sum_to(5)
+
+  # CHALLENGE 2
+# Write a function named largest that takes a list of numbers as an argument and returns the largest number in that list.
+
+# largest([1, 2, 3, 4, 0])  returns 4
+# largest([10, 4, 2, 231, 91, 54]) returns 231
+
+# list_1 = [1, 2, 3, 4, 0]
+# largest_number = 0
+
+# def largest(list):
+#   max = list[0]
+#   for num in list:
+#     if num > max:
+#       max = num
+#   return max
+
+# print(largest(list_1))
+
+
+#CHALLENGE 3
+# Write a function named occurances that takes two string arguments as input and counts the number of occurances of the second string inside the first string.
+
+# txt = "I love apples, apple are my favorite fruit"
+# x = txt.count("apple")
+
+# print(x)
+
+
+
+
+# CHALLENGE #4
+# Write a function named product that takes an arbitrary number of numbers, multiplies them all together, and returns the product.
+# (HINT: Review your notes on *args).
+
+# def product(*args):
+#     product = (-1, 4)
+#     for item in args:
+#         product *= item
+#     return product
+       
+
+# print(product(4))
